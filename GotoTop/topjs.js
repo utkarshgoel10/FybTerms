4dc9539f4a7bb4a545533f0a95baf60a1dcf848f
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2021-05-14 18:53:48 +0530

    Add files via upload

---
+++ GotoTop/topjs.js
@@ -0,0 +1,22 @@
+
+  $(function () {
+    $('#go-top').goTop({
+      scrollTop: 100,
+      scrollSpeed: 1000,
+      fadeInSpeed: 1000,
+      fadeOutSpeed: 500
+    })
+  })
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
